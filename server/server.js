69d69fb93c133b2656ab0a5b1dbd973b29a7a175
mini150146 <mini150146@gmail.com>
2019-08-26 21:58:05 +0900

    :see_no_evil: create secret.js

--- server/server.js
+++ server/server.js
@@ -3,20 +3,12 @@ const app = express();
 const http = require("http").createServer(app);
 const PORT = process.env.NODE_ENV === "production" ? 3001 : 3002;
 const cors = require("cors");
-const morgan = require("morgan");
+//const morgan = require("morgan");
 var router = require("./routes");
 var db = require("./db/models");
 
-const miniMorgan = (request, response, next) => {
-  console.log(
-    `리퀘스트 메소드: ${request.method}, 리퀘스트 url: ${request.url}`
-  );
-  next();
-};
-
 app.use(cors());
 app.use(express.json());
-app.use(miniMorgan);
 app.use("/", router);
 
 app.get("/", (req, res) => {
