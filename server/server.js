a3efc1d30c4ea6d0d225e6bf3455eef37f1813cd
mini150146 <mini150146@gmail.com>
2019-08-26 22:01:00 +0900

    :art: modify server.js

--- server/server.js
+++ server/server.js
@@ -3,13 +3,11 @@ const app = express();
 const http = require("http").createServer(app);
 const PORT = process.env.NODE_ENV === "production" ? 3001 : 3002;
 const cors = require("cors");
-//const morgan = require("morgan");
 var router = require("./routes");
 var db = require("./db/models");
 
-app.use(cors());
-app.use(express.json());
-app.use("/", router);
+app.use(cors()); // cors 에러 처리
+app.use("/", router); // 라우터
 
 app.get("/", (req, res) => {
   res.send("ok");
